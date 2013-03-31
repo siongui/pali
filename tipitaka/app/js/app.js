2aeebef20b68e2c8bd3837977c20b7ce580edc03
siongui <siongui@gmail.com>
2013-03-31 19:28:22 +0800

    re-arrange url routing on client side

--- tipitaka/app/js/app.js
+++ tipitaka/app/js/app.js
@@ -7,25 +7,23 @@ angular.module('paliTipitaka', ['paliTipitaka.services', 'paliTipitaka.directive
   }]).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/:urlLocale/canon/*canonPath/en_US/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
-    $routeProvider.when('/canon/*canonPath/en_US/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
     $routeProvider.when('/:urlLocale/canon/*canonPath/zh_TW/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
-    $routeProvider.when('/canon/*canonPath/zh_TW/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
     $routeProvider.when('/:urlLocale/canon/*canonPath/zh_CN/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
+    $routeProvider.when('/canon/*canonPath/en_US/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
+    $routeProvider.when('/canon/*canonPath/zh_TW/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
     $routeProvider.when('/canon/*canonPath/zh_CN/:translator/ContrastReading', {templateUrl: '/partials/canon.html', controller: contrastReadingCtrl});
     $routeProvider.when('/:urlLocale/canon/*canonPath/en_US/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
-    $routeProvider.when('/canon/*canonPath/en_US/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
     $routeProvider.when('/:urlLocale/canon/*canonPath/zh_TW/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
-    $routeProvider.when('/canon/*canonPath/zh_TW/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
     $routeProvider.when('/:urlLocale/canon/*canonPath/zh_CN/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
+    $routeProvider.when('/canon/*canonPath/en_US/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
+    $routeProvider.when('/canon/*canonPath/zh_TW/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
     $routeProvider.when('/canon/*canonPath/zh_CN/:translator', {templateUrl: '/partials/canon.html', controller: translationCtrl});
     $routeProvider.when('/:urlLocale/canon/*canonPath', {templateUrl: '/partials/canon.html', controller: canonCtrl});
-    $routeProvider.when('/canon/*canonPath', {templateUrl: '/partials/canon.html', controller: canonCtrl});
     $routeProvider.when('/:urlLocale/canon', {templateUrl: '/partials/canon.html', controller: canonCtrl});
+    $routeProvider.when('/canon/*canonPath', {templateUrl: '/partials/canon.html', controller: canonCtrl});
     $routeProvider.when('/canon', {templateUrl: '/partials/canon.html', controller: canonCtrl});
-    $routeProvider.when('/en_US/', {templateUrl: '/partials/info.html', controller: infoCtrl});
-    $routeProvider.when('/zh_TW/', {templateUrl: '/partials/info.html', controller: infoCtrl});
-    $routeProvider.when('/zh_CN/', {templateUrl: '/partials/info.html', controller: infoCtrl});
     $routeProvider.when('/', {templateUrl: '/partials/info.html', controller: infoCtrl});
+    $routeProvider.when('/:urlLocale/', {templateUrl: '/partials/info.html', controller: infoCtrl});
     $routeProvider.otherwise({redirectTo: '/'});
   }]).
   run(['$rootScope', '$location', '$document', 'i18nserv', 'resizableViews', function($rootScope, $location, $document, i18nserv, resizableViews) {
