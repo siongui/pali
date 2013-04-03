5052a6f2341f7b99e1b85be7a1a612bb879945f1
siongui <siongui@gmail.com>
2013-04-04 06:16:24 +0800

    translation of 教化仙伽邏經 (DN 31) Siṅgālasuttaṃ of 蕭式球

--- tipitaka/gaelibs/translationData.py
+++ tipitaka/gaelibs/translationData.py
@@ -9,6 +9,7 @@ translationInfo = {
     'canon': {
       's0101m.mul1.xml': ['3'],
       's0102m.mul8.xml': ['3'],
+      's0103m.mul7.xml': ['3'],
       's0201m.mul0.xml': ['3'],
       's0201m.mul1.xml': ['3'],
       's0201m.mul2.xml': ['3'],
