4b73a27622e0211a391363bde09624523e0c262d
siongui <siongui@gmail.com>
2013-04-09 03:06:54 +0800

    translation of Pakiṇṇakavaggo (Miscellany) Dhammapada by Ṭhānissaro Bhikkhu

--- tipitaka/gaelibs/translationData.py
+++ tipitaka/gaelibs/translationData.py
@@ -76,7 +76,8 @@ translationInfo = {
       's0502m.mul16.xml': ['1'],
       's0502m.mul17.xml': ['1'],
       's0502m.mul18.xml': ['1'],
-      's0502m.mul19.xml': ['1']
+      's0502m.mul19.xml': ['1'],
+      's0502m.mul20.xml': ['1']
     },
     'source': {
       '1': ['Ṭhānissaro Bhikkhu', 'http://www.accesstoinsight.org/tipitaka/translators.html#than', 'http://www.accesstoinsight.org/lib/authors/thanissaro/dhammapada.pdf']
@@ -289,6 +290,7 @@ canonTextTranslation['en_US'] = {
       'Malavaggo': 'Impurities',
       'Dhammaṭṭhavaggo': 'The Judge',
       'Maggavaggo': 'The Path',
+      'Pakiṇṇakavaggo': 'Miscellany',
     'Udānapāḷi': 'Exclamations',
     'Itivuttakapāḷi': 'The Thus-saids',
     'Suttanipātapāḷi': 'The Discourses Collection',
