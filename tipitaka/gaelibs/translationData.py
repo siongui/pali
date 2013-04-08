cd6fa90534ad3fc8435981e6e9c606f518641007
siongui <siongui@gmail.com>
2013-04-08 19:19:42 +0800

    translation of Paṇḍitavaggo (The Wise) Dhammapada of Ṭhānissaro Bhikkhu

--- tipitaka/gaelibs/translationData.py
+++ tipitaka/gaelibs/translationData.py
@@ -61,7 +61,8 @@ translationInfo = {
       's0502m.mul1.xml': ['1'],
       's0502m.mul2.xml': ['1'],
       's0502m.mul3.xml': ['1'],
-      's0502m.mul4.xml': ['1']
+      's0502m.mul4.xml': ['1'],
+      's0502m.mul5.xml': ['1']
     },
     'source': {
       '1': ['Ṭhānissaro Bhikkhu', 'http://www.accesstoinsight.org/tipitaka/translators.html#than', 'http://www.accesstoinsight.org/lib/authors/thanissaro/dhammapada.pdf']
@@ -259,6 +260,7 @@ canonTextTranslation['en_US'] = {
       'Cittavaggo': 'The Mind',
       'Pupphavaggo': 'Blossoms',
       'Bālavaggo': 'Fools',
+      'Paṇḍitavaggo': 'The Wise',
     'Udānapāḷi': 'Exclamations',
     'Itivuttakapāḷi': 'The Thus-saids',
     'Suttanipātapāḷi': 'The Discourses Collection',
