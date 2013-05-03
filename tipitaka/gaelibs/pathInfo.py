42718cae7db16891f8c6b79eff0115681df168f1
siongui <siongui@gmail.com>
2013-05-03 12:19:26 +0800

    add comment documentation

--- tipitaka/gaelibs/pathInfo.py
+++ tipitaka/gaelibs/pathInfo.py
@@ -3,12 +3,31 @@
 
 import os, json
 
+# http://stackoverflow.com/questions/3898572/what-is-the-standard-python-docstring-format
+# http://stackoverflow.com/questions/2447109/showing-a-different-background-colour-in-vim-past-80-characters
+"""format of variable in treeviewAll.json
+
+The variable in treeviewAll.json is a Python dict (JavaScript object) which is:
+    { tipitakaNode, atthakathaNode, tikaNode, anyaNode }
+where each node contains the following attribute:
+    Non-leaf node:
+        { 'text': string ,
+          'child': [ node, node, ... ],
+          'subpath': string }
+    Leaf node:
+        { 'text': string ,
+          'action': string,
+          'subpath': string }
+This is a tree structure.
+"""
 with open(os.path.join(os.path.dirname(__file__), 'json/treeviewAll.json'), 'r') as f:
   treeviewData = json.loads(f.read())
 
+# See translationInfo.py
 with open(os.path.join(os.path.dirname(__file__), 'json/translationInfo.json'), 'r') as f:
   translationInfo = json.loads(f.read())
 
+# cache of {xmlFilename, paliTextPath} pairs
 xmlFilename2PathInfo = {}
 
 
@@ -47,6 +66,43 @@ def isValidPaliTextPath(paliTextPath):
 
 
 def isValidPath(paliTextPath, translationLocale=None, translator=None):
+  """Check if the path of URL is valid.
+
+  The path of URL could be one of the following:
+      {{paliText}}
+      /{{urlLocale}}{{paliTextPath}}
+      /{{urlLocale}}{{paliTextPath}}/{{translationLocale}}/{{translator}}
+      /{{urlLocale}}{{paliTextPath}}/{{translationLocale}}/{{translator}}/ContrastReading
+  Before the path is being passed in this function, it has already been filtered
+  by Python web framework like webapp2 or web.py, so {{urlLocale}} has been
+  stripped.
+
+  Args:
+      paliTextPath: for example:
+          /canon
+          /canon/sutta
+          /canon/sutta/dīgha
+          /canon/sutta/dīgha/sīlakkhandhavagga
+          /canon/sutta/dīgha/sīlakkhandhavagga/sāmaññaphalasuttaṃ
+
+      translationLocale: particular language translation of pali text of
+          {{paliTextPath}}, for example:
+          en_US: English translation of pali text of {{paliTextPath}}
+          zh_TW: Traditional Chinese translation of pali text of 
+              {{paliTextPath}}
+
+      translator: name of the translator
+
+  Returns:
+      A dict which contains the following information if the path is valid:
+
+       { 'isValid': True,
+         'node': one of the node of the variable in treeviewAll.json,
+                 which corresponds to {{paliTextPath}},
+         'texts': [ node1['text'], ... ] }
+
+      If not valid path, return { 'isValid': False }
+  """
   result = isValidPaliTextPath(paliTextPath)
   if result['isValid'] and translationLocale:
     if 'action' in result['node']:
