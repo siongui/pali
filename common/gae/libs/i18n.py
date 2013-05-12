72eb44bb88f9a80161071218571d28a34b788840
siongui <siongui@gmail.com>
2013-05-12 08:36:39 +0800

    make i18n module thread safe

--- common/gae/libs/i18n.py
+++ common/gae/libs/i18n.py
@@ -8,34 +8,37 @@ http://jinja.pocoo.org/docs/extensions/
 http://webpy.org/cookbook/i18n_support_in_template_file
 http://webpy.org/cookbook/runtime-language-switch
 https://code.google.com/p/webapp-improved/source/browse/webapp2_extras/i18n.py
+http://docs.python.org/2/library/threading.html
 """
 
 import os
 import gettext
+import threading
 
 localedir = os.path.join(os.path.dirname(__file__), '../../locale')
 domain = 'messages'
+threadLocalData = threading.local()
+threadLocalData.locale = 'en_US'
 
 AllTranslations = {
 'en_US': gettext.translation(domain, localedir, ['en_US']),
 'zh_TW': gettext.translation(domain, localedir, ['zh_TW']),
-'zh_CN': gettext.translation(domain, localedir, ['zh_CN']),
-'locale': 'en_US'
+'zh_CN': gettext.translation(domain, localedir, ['zh_CN'])
 }
 
 def gettext(message):
-  return AllTranslations[ AllTranslations['locale'] ].gettext(message)
+  return AllTranslations[ threadLocalData.locale ].gettext(message)
 
 def ugettext(message):
-  return AllTranslations[ AllTranslations['locale'] ].ugettext(message)
+  return AllTranslations[ threadLocalData.locale ].ugettext(message)
 
 def ngettext(singular, plural, n):
-  return AllTranslations[ AllTranslations['locale'] ].ngettext(singular, plural, n)
+  return AllTranslations[ threadLocalData.locale ].ngettext(singular, plural, n)
 
 def ungettext(singular, plural, n):
-  return AllTranslations[ AllTranslations['locale'] ].ungettext(singular, plural, n)
+  return AllTranslations[ threadLocalData.locale ].ungettext(singular, plural, n)
 
 def setLocale(locale):
   if locale in ['en_US', 'zh_TW', 'zh_CN']:
-    AllTranslations['locale'] = locale
+    threadLocalData.locale = locale
 
