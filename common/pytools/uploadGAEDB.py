1a093af55205f0220192715a51bc0669ad4d4829
siongui <siongui@gmail.com>
2013-05-17 05:20:22 +0800

    add comments and rename function and variable

--- common/pytools/uploadGAEDB.py
+++ common/pytools/uploadGAEDB.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# For uploading data to Google App Engine datastore
+# For uploading data to Google App Engine datastore or blobstore
 
 import os
 import sys
 import json
 
 SDK_PATH = os.path.expanduser("~/google_appengine/")
-
+"""
+cannot use:
+    sys.path.append(SDK_PATH)
+must use:
+    sys.path.insert(0, SDK_PATH)
+"""
 sys.path.insert(0, SDK_PATH)
 sys.path.append(os.path.join(SDK_PATH, 'lib/yaml/lib'))
 sys.path.append(os.path.join(SDK_PATH, 'lib/fancy_urllib'))
@@ -24,12 +29,17 @@ remote_api_stub.ConfigureRemoteApi(None, '/_ah/remote_api', auth_func,
                                    'epalitipitaka.appspot.com')
 
 class Xml(ndb.Model):
+  # maybe can try optional keyword argument "compressed". See
+  # https://developers.google.com/appengine/docs/python/ndb/properties#compressed
   content = ndb.BlobProperty()
 
 
 def uploadXmls():
+  """Upload all xml files to the datastore of production server programmatically
+     via remote api.
+  """
   romn_dir = os.path.join(os.path.dirname(__file__), '../romn/')
-  fail_to_uploaded_xmls = []
+  fail_to_be_uploaded_xmls = []
 
   for dirpath, dirnames, filenames in os.walk(romn_dir):
     for filename in filenames:
@@ -37,17 +47,20 @@ def uploadXmls():
       key = path.lstrip(romn_dir)
       print('uploading %s ...' % key)
       with open(path, 'rb') as f:
+        # because of "1 MB API limits apply" of remote_api, see
+        # https://developers.google.com/appengine/articles/remote_api#limitations
+        # The following code may raise exception if request over 1 MB
         try:
           Xml(id=key, content=f.read()).put()
         except:
-          fail_to_uploaded_xmls.append(key)
+          fail_to_be_uploaded_xmls.append(key)
           print('fail to upload %s' % key)
 
   with open(os.path.join(os.path.dirname(__file__), 'fail.json'), 'w') as f:
-    f.write(json.dumps(fail_to_uploaded_xmls))
+    f.write(json.dumps(fail_to_be_uploaded_xmls))
 
 
-def deleteAll():
+def deleteAllXmlModel():
   ndb.delete_multi(Xml.query().fetch(999999, keys_only=True))
 
 
@@ -86,6 +99,6 @@ def uploadXmlsToBlobstore():
 
 
 if __name__ == '__main__':
-  #deleteAll()
+  #deleteAllXmlModel()
   #uploadXmls()
-  uploadXmlsToBlobstore()
+  #uploadXmlsToBlobstore()
