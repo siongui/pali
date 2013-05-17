f178112e8bca838411eeb5e7fafee6997fe32331
siongui <siongui@gmail.com>
2013-05-17 15:38:36 +0800

    use blobstore to read xml files

--- tipitaka/gaelibs/xml2html.py
+++ tipitaka/gaelibs/xml2html.py
@@ -5,65 +5,48 @@ import os
 from lxml import etree
 from translationInfo import getTranslatorSource
 
+from google.appengine.ext import ndb
+from google.appengine.ext import blobstore
 
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../common/gae/libs'))
-from misc import isGoogleAppEngine
-from misc import isProductionServer
+class XmlBlobKey(ndb.Model):
+  blob_key = ndb.BlobKeyProperty()
 
-if isGoogleAppEngine():
-  import urllib2
-  if isProductionServer():
-    paliXmlUrlPrefix = u'http://epalitipitaka.appspot.com/romn/'
-    trXmlUrlPrefix = u'http://epalitipitaka.appspot.com/translation/'
-  else:
-    #paliXmlUrlPrefix = os.path.join(os.path.dirname(__file__), 'romn')
-    #trXmlUrlPrefix = os.path.join(os.path.dirname(__file__), 'translation')
-    paliXmlUrlPrefix = u'http://localhost:8080/romn/'
-    trXmlUrlPrefix = u'http://localhost:8080/translation/'
-else:
-  # add amazon ec2 support here
-  raise Exception('currently only Google App Engine is supported')
+#paliXmlUrlPrefix = os.path.join(os.path.dirname(__file__), 'romn')
+trXmlUrlPrefix = os.path.join(os.path.dirname(__file__), 'translation')
 
-
-if isGoogleAppEngine():
-  result = urllib2.urlopen(os.path.join(
-             paliXmlUrlPrefix, 'cscd/tipitaka-latn.xsl'))
-  xslt_root = etree.parse(result)
-  transform = etree.XSLT(xslt_root)
-else:
-  # add amazon ec2 support here
-  """
-  with open(os.path.join(paliXmlUrlPrefix, 'cscd/tipitaka-latn.xsl'), 'r') as f:
-    xslt_root = etree.parse(f)
-  transform = etree.XSLT(xslt_root)
-  """
-  raise Exception('currently only Google App Engine is supported')
+xslt_root = etree.parse(blobstore.BlobReader(
+    XmlBlobKey.get_by_id('cscd/tipitaka-latn.xsl').blob_key))
+transform = etree.XSLT(xslt_root)
 
 
 def getCanonXmlUrl(action):
+  """deprecated"""
   return os.path.join(paliXmlUrlPrefix, action)
 
+
 def getTranslationXmlUrl(action, translationLocale, translator):
+  """Determine the path of translation xml file.
+
+  Returns:
+      Path string
+  """
   xmlFilename = os.path.basename(action)
   code = getTranslatorSource(xmlFilename, translationLocale, translator)
   return os.path.join(trXmlUrlPrefix, '%s/%s/%s' % (
                                        translationLocale, code, xmlFilename))
 
-def xslt(url):
-  if isGoogleAppEngine():
-    result = urllib2.urlopen(url)
-    root = etree.parse(result)
-  else:
-    # add amazon ec2 support here
-    raise Exception('currently only Google App Engine is supported')
+
+def xslt(fileLikeObject):
+  root = etree.parse(fileLikeObject)
   # transform xml with xslt
   return transform(root)
 
 
 def paliXslt(action):
-  return xslt(getCanonXmlUrl(action))
+  return xslt(blobstore.BlobReader(XmlBlobKey.get_by_id(action).blob_key))
 
 
 def translationXslt(action, translationLocale, translator):
-  return xslt(getTranslationXmlUrl(action, translationLocale, translator))
+  with open(getTranslationXmlUrl(action, translationLocale, translator),
+     'r') as f:
+    return xslt(f)
