f4d473d9ff6299a97cb6b0434f103a8e998be08e
siongui <siongui@gmail.com>
2013-05-20 20:30:28 +0800

    test new data

---
+++ dictionary/gaelibs/url2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import jinja2
+import json
+
+jj2env = jinja2.Environment(
+  loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+with open(os.path.join(os.path.dirname(__file__), 'books.json'), 'r') as f:
+  dicIndex = json.loads(f.read())
+
+def getWordHtml(prefix, word, urlLocale):
+  with open(os.path.join(os.path.dirname(__file__), 'dhamma'), 'r') as f:
+    jsonData = f.read()
+
+  template = jj2env.get_template('word2.html')
+  return template.render({'bookExps': json.loads(jsonData),
+                          'booksIndex': dicIndex,
+                          'word': word,
+                          'urlLocale': urlLocale})
+
