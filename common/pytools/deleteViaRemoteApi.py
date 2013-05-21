c5eed04482f6f641bb6df1f18246b438b655941f
siongui <siongui@gmail.com>
2013-05-21 12:21:35 +0800

    update script for delete datastore entities via remote api

--- common/pytools/deleteViaRemoteApi.py
+++ common/pytools/deleteViaRemoteApi.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import os
+import sys
+SDK_PATH = os.path.expanduser("~/google_appengine/")
+sys.path.insert(0, SDK_PATH)
+sys.path.append(os.path.join(SDK_PATH, 'lib/yaml/lib'))
+sys.path.append(os.path.join(SDK_PATH, 'lib/fancy_urllib'))
+from google.appengine.ext.remote_api import remote_api_stub
 from google.appengine.ext import ndb
+import getpass
+
+def auth_func():
+  return (raw_input('Username:'), getpass.getpass('Password:'))
+
+remote_api_stub.ConfigureRemoteApi(None, '/_ah/remote_api', auth_func,
+                                   'palidictionary.appspot.com')
 
 class PaliWordJson(ndb.Model):
   """data is in json-format (deprecated)"""
@@ -17,8 +31,19 @@ class PaliWord(ndb.Model):
   xmlfilename = ndb.StringProperty()
   xmlfiledata = ndb.TextProperty()
 
-def deleteAllXmlModel():
-  """FIXME: The following command cannot be used via remote api of production
-     server, because it will run out of free quota!!!
+def deleteXmlModel(num):
+  """num cannot be too large (will run out of free quota immediately),
+     num=100 is a good fit.
   """
-  ndb.delete_multi(Xml.query().fetch(999999, keys_only=True))
+  ndb.delete_multi(Xml.query().fetch(num, keys_only=True))
+
+def deletePaliWordModel(num):
+  """num cannot be too large (will run out of free quota immediately),
+     num=100 is a good fit.
+  """
+  ndb.delete_multi(PaliWord.query().fetch(num, keys_only=True))
+
+
+if __name__ == '__main__':
+  #deleteXmlModel(100)
+  deletePaliWordModel(100)
