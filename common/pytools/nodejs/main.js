a9887cfbdf7c9afcbcad49d242102621a5204789
siongui <siongui@gmail.com>
2013-05-25 12:37:34 +0800

    use interactive commandline interface to look up words

--- common/pytools/nodejs/main.js
+++ common/pytools/nodejs/main.js
@@ -40,7 +40,18 @@ console.log(output);
 var json = eval( '(' + output + ")" );
 var ftrie = new bitsjs.FrozenTrie( json.trie, json.directory, json.nodeCount);
 
-console.log('looking up ḍīyate ...');
-console.log(ftrie.lookup('ḍīyate'));
-console.log('looking up ḍīyata ...');
-console.log(ftrie.lookup('ḍīyata'));
+// @see http://nodejs.org/api/readline.html#readline_example_tiny_cli
+var readline = require('readline'),
+    rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('word> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+  console.log('looking up ' + line.trim() + ' ...' );
+  console.log(ftrie.lookup(line.trim()));
+  rl.prompt();
+}).on('close', function() {
+  console.log('\nEnd of lookup');
+  process.exit(0);
+});
