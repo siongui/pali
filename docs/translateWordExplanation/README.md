ba2a96e50e991696a2ea87d5f56324badb508f5d
siongui <siongui@gmail.com>
2013-06-02 11:48:47 +0800

    demo code of translate explanation of words

---
+++ docs/translateWordExplanation/README.md
@@ -0,0 +1,19 @@
+# Development
+
+<i>PALI_DIR</i> below means the directory where you git clone <em>pali</em> repository.
+
+1. git clone the [pali](https://github.com/siongui/pali) repository and [data](https://github.com/siongui/data) repository (put in the same directory).
+```bash
+    # create a directory to contain both pali and data repository.
+    $ mkdir dev
+    $ cd dev
+    # git clone repositories
+    $ git clone https://github.com/siongui/pali.git
+    $ git clone https://github.com/siongui/data.git
+```
+
+2. Install [goslate](https://pypi.python.org/pypi/goslate)
+```bash
+    $ sudo pip install goslate
+```
+
---
+++ docs/translateWordExplanation/translate.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import json
+import csv
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../common/pytools'))
+from variables import getDictBooksJsonPath
+from variables import getDictWordsCSV1Path
+from variables import getDictWordsCSV2Path
+
+import goslate
+from time import sleep
+
+
+def extractOneDicExps(dicIndex, dicId):
+  with open(getDictWordsCSV1Path(), 'r') as f:
+    wordreader = csv.reader(f, delimiter=',', quotechar='"')
+    for row in wordreader:
+      if len(row) != 7:
+        raise Exception('len(row) != 7')
+      if row[0] == 'db_id': continue
+
+      word = row[4].decode('utf-8').lower()
+      if row[2] == dicId:
+        yield [word, row[6]]
+
+  with open(getDictWordsCSV2Path(), 'r') as f:
+    wordreader = csv.reader(f, delimiter=',', quotechar='"')
+    for row in wordreader:
+      if len(row) != 7:
+        raise Exception('len(row) != 7')
+      if row[0] == 'db_id': continue
+
+      word = row[4].decode('utf-8').lower()
+      if row[2] == dicId:
+        yield [word, row[6]]
+
+
+if __name__ == '__main__':
+  gs = goslate.Goslate()
+  #print(gs.get_languages())
+
+  with open(getDictBooksJsonPath(), 'r') as f:
+    dicIndex = json.loads(f.read())
+
+  """
+  Pali-English Id:
+    "N": "Buddhist Dictionary by NYANATILOKA MAHATHERA"
+    "C": "Concise Pali-English Dictionary by A.P. Buddhadatta Mahathera"
+    "P": "PTS Pali-English dictionary The Pali Text Society's Pali-English dictionary"
+    "V": "Buddhist Dictionary of Pali Proper Names by G P Malalasekera"
+    "I": "Pali-Dictionary Vipassana Research Institute"
+  """
+  dicId = 'C'
+
+  index = 0
+  for word, explanation in extractOneDicExps(dicIndex, dicId):
+    index += 1
+    if index < 22560: continue
+    print(explanation)
+    print(gs.translate(explanation, 'fr', 'en'))
+    sleep(0.9)
+
+  print('number of words in %s: %d' % (dicIndex[dicId][3], index + 1))
