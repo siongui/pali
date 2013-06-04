f563249b614f2b47bf4daa08eb4f9e43c91a9329
siongui <siongui@gmail.com>
2013-06-04 20:25:43 +0800

    read word json from local filesystem if not GAE

--- dictionary/gaelibs/wordJson.py
+++ dictionary/gaelibs/wordJson.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from google.appengine.ext import ndb
+try:
+  from google.appengine.ext import ndb
 
-class PaliWordJsonBlob(ndb.Model):
-  """blob which stores json data"""
-  data = ndb.BlobProperty()
+  class PaliWordJsonBlob(ndb.Model):
+    """blob which stores json data"""
+    data = ndb.BlobProperty()
 
-def readWordFromGAEDatastore(word):
-  jblob = PaliWordJsonBlob.get_by_id(word)
-  if jblob:
-    return jblob.data
-  else:
-    raise Exception('word not found: %s' % word)
+  def readWordJson(word):
+    """read word json from GAE datastore"""
+    jblob = PaliWordJsonBlob.get_by_id(word)
+    if jblob:
+      return jblob.data
+    else:
+      raise Exception('word not found: %s' % word)
+
+except ImportError:
+  import os
+
+  wordsDir = os.path.join(os.path.dirname(__file__), 'paliwords')
+
+  def readWordJson(word):
+    path = os.path.join(wordsDir, word.decode('utf-8'))
+    with open(path, 'r') as f:
+      return f.read()
+
 
 def getWordJson(word):
-  return readWordFromGAEDatastore(word)
+  return readWordJson(word)
