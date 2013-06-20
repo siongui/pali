8b74d252512d058b6ce6890ec9157be576e66644
siongui <siongui@gmail.com>
2013-06-20 23:18:41 +0800

    better code structure for creating translation trees

--- tipitaka/setup/translationTreesToHtml.py
+++ tipitaka/setup/translationTreesToHtml.py
@@ -14,24 +14,26 @@ jj2env = jinja2.Environment(
     variable_end_string='$}')
 
 
+def getChildren(node, ngVar, locale):
+  container = etree.fromstring(
+      '<div ng-hide="%s" class="childrenContainer"></div>' % ngVar)
+
+  index = 0
+  for child in node['child']:
+    container.append( translationTreeToHtml(child, ngVar, index, locale) )
+    index += 1
+
+  return container
+
 def translationTreeToHtml(tree, prefix, index, locale):
   if 'text' not in tree:
     # root tree
-    ngVar = "show%s" % locale
+    ngVar = "%sng" % locale
 
     template = jj2env.get_template('rootNode.html')
     root = etree.fromstring(
         template.render( {'ngVar': ngVar, 'locale': locale } ) )
-
-    childrenContainer = etree.fromstring(
-        '<div ng-hide="%s" class="childrenContainer"></div>' % ngVar)
-    childIndex = 0
-    for child in tree['child']:
-      childrenContainer.append( translationTreeToHtml(
-          child, '%sng' % locale, childIndex, locale) )
-      childIndex += 1
-
-    root.append(childrenContainer)
+    root.append( getChildren(tree, ngVar, locale) )
     return root
 
   else:
@@ -41,20 +43,8 @@ def translationTreeToHtml(tree, prefix, index, locale):
       template = jj2env.get_template('nonLeafNode.html')
       node = etree.fromstring(
           template.render( {'ngVar': ngVar, 'text': tree['text'] } ) )
-
-      childrenContainer = etree.fromstring(
-          '<div ng-hide="%s" class="childrenContainer"></div>' % ngVar)
-      childIndex = 0
-      for child in tree['child']:
-        childrenContainer.append(
-            translationTreeToHtml(child, ngVar, childIndex, locale) )
-        childIndex += 1
-
-      container = etree.fromstring('<div></div>')
-      container.append(node)
-      container.append(childrenContainer)
-
-      return container
+      node.append( getChildren(tree, ngVar, locale) )
+      return node
 
     else:
       template = jj2env.get_template('leafNode.html')
