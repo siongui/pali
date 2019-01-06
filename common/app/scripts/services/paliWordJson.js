3a197d45198f344ef1408fd8063696a19188b921
siongui <tesiongui@gmail.com>
2019-01-06 22:10:11 +0800

    use updated database for JSON data of pali words

--- common/app/scripts/services/paliWordJson.js
+++ common/app/scripts/services/paliWordJson.js
@@ -7,7 +7,8 @@ angular.module('pali.wordJson', []).
   factory('paliWordJson', ['$http', function($http) {
 
     function get(word) {
-      var url = '/wordJson/' + word;
+      //var url = '/wordJson/' + word;
+      var url = "https://siongui.github.io/xemaauj9k5qn34x88m4h/" + word + ".json";
       return $http.get(url, {cache: true});
     }
 
